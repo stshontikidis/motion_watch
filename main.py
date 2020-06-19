dacbedd5880c2aaffb9f941191ee5d4d71a031dc
Sava Tshontikidis <savatshontikidis@Savas-MacBook-Pro.local>
2020-06-19 18:52:39 -0400

    Added a main that handels starting a threading
    I want to continually tail the log but also sub/pub mqtt topcis so
    trying threading....

---
+++ main.py
@@ -0,0 +1,65 @@
+import json
+import time
+import threading
+
+import motion_watch
+import paho.mqtt.client as mqtt
+
+def main():
+
+    with open('config.json') as file:
+        config = json.load(file)
+
+    watcher = motion_watch.Watcher(config['log_file'], config['offset_file'])
+    watch_thread = threading.Thread(target=watcher.run)
+
+    # The callback for when the client receives a CONNACK response from the server.
+    def on_connect(client, userdata, flags, rc):
+        print("Connected with result code "+str(rc))
+
+        # reconnect then subscriptions will be renewed.
+        client.subscribe('switch/motion/baby_cam', 1)
+
+    def on_message(client, userdata, msg):
+        if msg.payload.decode('utf-8') == 'on':
+            if watch_thread.is_alive():
+                watcher.start()
+            else:
+                # need to find out how to handle excptions in that thread
+                try:
+                    watch_thread.start()
+                except:
+                    pass
+
+        elif msg.payload.decode('utf-8') == 'off' and watch_thread.is_alive():
+            watcher.stop()
+
+    def on_disconnect(client, userdata, rc):
+        pass
+
+    mqtt_client = mqtt.Client('motion_watch')
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_message = on_message
+    mqtt_client.username_pw_set(config['mqtt_user'], config['mqtt_password'])
+
+    mqtt_client.enable_logger()
+
+    mqtt_client.connect(config['mqtt_host'], config['mqtt_port'])
+    mqtt_client.loop_start()
+
+    try:
+        while True:
+            if watcher.alert and not watcher.has_alerted:
+                mqtt_client.publish('motion/alert/baby_cam', payload='on')
+                watcher.has_alerted = True
+
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        mqtt_client.loop_stop()
+        mqtt_client.disconnect()
+        watcher.stop()
+        watcher.exit = True
+
+    print('Exited gracefully!')
+if __name__ == "__main__":
+    main()
--- motion_watch.py
+++ motion_watch.py
@@ -1,39 +1,73 @@
 import datetime
-import json
+import os
 import re
-import sys
 import time
 
 import pygtail
 
+class Watcher:
 
-def main():
-    expression = r'Baby Cam] MotionEvent type:(\w+) event:(\d+)'
-    regex = re.compile(expression)
-    current_event = None
+    def __init__(self, log_file, offset_file):
+        self.alert = False
+        self.has_alerted = False
+        self.monitor = True
+        self.exit = False
+        self.offset_file = offset_file
 
-    alerted = False
-    while True:
-        for line in pygtail.Pygtail("/volume1/docker/unifi-video/logs/motion.log", offset_file='motion.offset', read_from_end=True):
+        self.log_file = log_file
+
+        expression = r'Baby Cam] MotionEvent type:(\w+) event:(\d+)'
+        self.regex = re.compile(expression)
+
+        self.current_event = None
+        self.start_time = None
+
+    def watch(self):
+        for line in pygtail.Pygtail(self.log_file, offset_file=self.offset_file, read_from_end=True):
             print(line)
-            match = regex.search(line)
+            match = self.regex.search(line)
 
             if match and match.group(1) == 'start':
-                current_event = match.group(2)
-                start_time = datetime.datetime.now()
+                self.current_event = match.group(2)
+                self.start_time = datetime.datetime.now()
+
+                self.has_alerted = False
+                self.alert = False
             elif match and match.group(1) == 'stop':
-                current_event = None
-                alerted = False
-        
-        print('current event {}'.format(current_event))
-        if current_event and not alerted:
-            time_diff = datetime.datetime.now() - start_time
-            print('checking again {}'.format(time_diff.total_seconds()))
-            if time_diff.total_seconds() > 20:
-                print('Alert!')
-                alerted = True
+                self.current_event = None
+                self.start_time = None
 
-        time.sleep(1)
+        # print('current event {}'.format(current_event))
+        if self.current_event and not self.alert and not self.has_alerted:
+            time_diff = datetime.datetime.now() - self.start_time
+            # print('checking again {}'.format(time_diff.total_seconds()))
+            if time_diff.total_seconds() > 10:
+                self.alert = True
 
-if __name__ == "__main__":
-    main()
+    def run(self):
+        self.monitor = True
+
+        while not self.exit:
+            if self.monitor:
+                self.watch()
+            time.sleep(1)
+
+    def start(self):
+        try:
+            os.remove(self.offset_file)
+        except FileNotFoundError:
+            pass
+
+        self.monitor = True
+        self.alert = False
+        self.has_alerted = False
+
+
+    def stop(self):
+        self.monitor = False
+
+        try:
+            os.remove(self.offset_file)
+        except FileNotFoundError:
+            pass
+
