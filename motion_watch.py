a78df3fd762b15452ab8996ae61744fad7a694eb
Sava Tshontikidis <savatshontikidis@Savas-MacBook-Pro.local>
2020-06-26 18:06:40 -0400

    Adding logging

--- motion_watch.py
+++ motion_watch.py
@@ -7,7 +7,7 @@ import pygtail
 
 class Watcher:
 
-    def __init__(self, log_file, offset_file):
+    def __init__(self, log_file, offset_file, logger):
         self.alert = False
         self.has_alerted = False
         self.monitor = True
@@ -22,10 +22,17 @@ class Watcher:
         self.current_event = None
         self.start_time = None
 
+        self.logger = logger
+
     def watch(self):
         for line in pygtail.Pygtail(self.log_file, offset_file=self.offset_file, read_from_end=True):
-            print(line)
+            if line:
+                self.logger.debug(line)
+
             match = self.regex.search(line)
+
+            if match:
+                self.logger.debug('Match found {}'.format(match.group(0)))
 
             if match and match.group(1) == 'start':
                 self.current_event = match.group(2)
@@ -37,12 +44,15 @@ class Watcher:
                 self.current_event = None
                 self.start_time = None
 
-        # print('current event {}'.format(current_event))
+        if current_event:
+            self.logger.debug('current event {}'.format(current_event))
+
         if self.current_event and not self.alert and not self.has_alerted:
             time_diff = datetime.datetime.now() - self.start_time
-            # print('checking again {}'.format(time_diff.total_seconds()))
+            self.logger.debug('checking event {} again {}'.format(current_event, time_diff.total_seconds()))
             if time_diff.total_seconds() > 10:
                 self.alert = True
+                self.logger.debug('Alert threshold hit for event {}'.format(current_event))
 
     def run(self):
         self.monitor = True
